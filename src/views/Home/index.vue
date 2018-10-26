0733f6f2716409026e0a0488cd46cf8c9bd8f17b
heroes007 <1149961628@qq.com>
2018-10-26 17:46:17 +0800

    '测试'

---
+++ src/views/Home/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>首页</h1>
+    <router-link to="/second">第二页</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "index"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home/second.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>第二页</h1>
+    <router-link to="/">首页</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "second"
+  }
+</script>
+
+<style scoped>
+
+</style>
